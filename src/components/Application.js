7ba27d4ffd0ff55c5f031dbd042025f3996f9b90
SophiaL1024 <sophiafengliu@gmail.com>
2021-06-10 12:31:29 -0700

    fetch interviewers

--- src/components/Application.js
+++ src/components/Application.js
@@ -12,7 +12,8 @@ export default function Application() {
   const [state, setState] = useState({
     day: "",
     days: [],
-    appointments:{}
+    appointments:{},
+    interviewers :{}
   });
 
   
@@ -21,25 +22,27 @@ export default function Application() {
   
   useEffect(() => {
     
-    //define functions to set days and set appointments 
+    //define functions to set days, set appointments and set interviewers
     const setDays = days => setState(prev => ({ ...prev, days }));;
     const setAppointments = appointments=> setState(prev => ({ ...prev, appointments }));;
+    const setInterviewers = interviewers=> setState(prev => ({ ...prev, interviewers }));;
     
     Promise.all([
       axios.get("http://localhost:8001/api/days"),
       axios.get(" http://localhost:8001/api/appointments"),
-      
+      axios.get(" http://localhost:8001/api/interviewers")      
     ])
     .then((all) => {
       setDays(all[0].data);
       setAppointments(all[1].data);
+      setInterviewers(all[2].data);
     })
     
     
   }, []);
   
   const dailyAppointments = getAppointmentsForDay(state,state.day);
-  // const dailyAppointments=[];
+
 
   return (
     <main className="layout">
